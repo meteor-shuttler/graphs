8c61cc454899edb2daa873d9b65cbc517f19c646
Ivan S Glazunov <ivansglazunov@gmail.com>
2016-03-09 20:17:31 +0300

    0.0.11

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'shuttler:graphs',
-  version: '0.0.10',
+  version: '0.0.11',
   summary: 'Oriented graph methods with shema and restrictions.',
   git: 'https://github.com/meteor-shuttler/graphs',
   documentation: 'README.md'
